ddbc53d0280c9ff4e6856b3c2bdc4ca42c04b561
sagardnale <nalesagard@gmail.com>
2025-08-22 12:43:47 +0530

    created routing url for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AccountComponent } from './pages/account/account.component';
+import { LogoutComponent } from './pages/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'reports',component:ReportsComponent},
+  {path:'settings',component:SettingsComponent,children:[
+    {path:'profile',component:ProfileComponent},
+    {path:'account',component:AccountComponent},
+    {path:'logout',component:LogoutComponent}
+  ]},
+  {path:'**',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
