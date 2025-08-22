e86fcedc35182de7af3df421ca64e594117c8554
sagardnale <nalesagard@gmail.com>
2025-08-22 12:39:27 +0530

    create pages like home,reports,settings,profile,account,logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AccountComponent } from './pages/account/account.component';
+import { LogoutComponent } from './pages/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReportsComponent,
+    SettingsComponent,
+    ProfileComponent,
+    AccountComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
