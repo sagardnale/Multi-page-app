e90422267f104fefbfde13cbab6c07e8c144f8b4
sagardnale <nalesagard@gmail.com>
2025-08-23 19:34:41 +0530

    created menu bar with responsive for mobile,tab,large and extra large screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('fadeAnimation',[
+      transition('* <=> *',[
+        query(':enter, :leave',[
+          style({ position: 'absolute', width:'100%'})
+        ],{optional:true}),
+        group([
+          query(':enter',[
+            style({opacity:0}),
+            animate('400ms ease-out',style({ opacity:1}))
+          ],{ optional: true}),
+          query(':leave',[
+            animate('400ms ease-out',style({ opacity: 0}))
+          ],{optional: true})
+        ])
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'multi-page-app';
+  getRouterOutletState(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
