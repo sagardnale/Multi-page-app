413d94de53c73c4b20fa5d2a88a266e994d9a473
sagardnale <nalesagard@gmail.com>
2025-08-25 12:28:32 +0530

    added login and logout functionality in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly AUTH_KEY = 'isLoggedIn';
 
-  constructor() { }
+  constructor(private readonly router: Router) { }
+
+  login(email:string,password:string):boolean{
+    if(email && password){
+      localStorage.setItem(this.AUTH_KEY,'true');
+      return true;
+    }
+    return false;
+  }
+
+  logout(){
+    localStorage.removeItem(this.AUTH_KEY);
+    this.router.navigate(['/login']);
+  }
+  
 }
