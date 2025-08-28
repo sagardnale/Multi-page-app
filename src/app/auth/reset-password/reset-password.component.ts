a0c1eb3bf40aa34ed26076fe12adf03728f2efda
sagardnale <nalesagard@gmail.com>
2025-08-28 12:40:29 +0530

    created reactive form for reset password page with validators

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
@@ -7,5 +8,24 @@ import { SharedService } from 'src/app/services/shared.service';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
-  constructor(public readonly shared:SharedService){}
+  resetPasswordForm! : FormGroup;
+  constructor(public readonly shared:SharedService, private readonly fb:FormBuilder){}
+
+  ngOnInit(){
+    this.resetPasswordForm = this.fb.group({
+      password:['',[Validators.required,Validators.minLength(6)]],
+      confirmPassword:['',[Validators.required]]
+    })
+  }
+
+  getResetPasswordFormControl(controlName:string){
+    return this.resetPasswordForm.get(controlName);
+  }
+  onResetPassword(){
+    if(this.resetPasswordForm.valid){
+      console.log("reset password forms values",this.resetPasswordForm.value);
+    }else{
+      this.resetPasswordForm.markAllAsTouched();
+    }
+  }
 }
