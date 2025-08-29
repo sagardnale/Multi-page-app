40e1513aa528bfced671657259c13c822f99cb35
sagardnale <nalesagard@gmail.com>
2025-08-29 10:22:30 +0530

    created toast service and added toast message to login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { SharedService } from 'src/app/services/shared.service';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-login',
@@ -18,22 +19,25 @@ export class LoginComponent {
     private readonly auth: AuthService,
     private readonly router: Router,
     public readonly shared: SharedService,
-    private readonly fb: FormBuilder
+    private readonly fb: FormBuilder,
+    private readonly toast: ToastService
   ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.loginForm = this.fb.group({
-      email:['',[Validators.required,Validators.email]],
-      password:['',[Validators.required,Validators.minLength(6)]]
-    })
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
-  getLoginControl(controlName:any){
+  getLoginControl(controlName: any) {
     return this.loginForm.get(controlName);
   }
   onSubmit() {
-    if(this.loginForm.valid){
-        console.log(this.loginForm.value);
+    if (this.loginForm.valid) {
+      this.toast.success('Logged in successfully!', 'Success');
+    } else {
+      this.toast.error('Please fix errors before submitting', 'Form Invalid');
     }
   }
 }
--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToastService {
+  constructor(private readonly toastr: ToastrService) {}
 
-  constructor(private readonly toastr: ToastService) { }
-  
-  success(message: string,title?:string){
-    this.toastr.success(message,title);
+  success(message: string, title?: string) {
+    this.toastr.success(message, title);
   }
-  error(message:string,title?:string){
-    this.toastr.error(message,title);
+  error(message: string, title?: string) {
+    this.toastr.error(message, title);
   }
-  info(message:string,title?:string){
-    this.toastr.error(message,title);
+  info(message: string, title?: string) {
+    this.toastr.error(message, title);
   }
-  warning(message:string,title?:string){
-    this.toastr.warning(message,title);
+  warning(message: string, title?: string) {
+    this.toastr.warning(message, title);
   }
 }
